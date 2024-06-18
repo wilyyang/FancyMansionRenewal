7b4922bd05f2629385a9d624df0b813aecd01aa8
wilyyang <ehdrnr1178@gmail.com>
2024-06-18 09:16:03 +0900

    theme font size 재정립

--- core/presentation/src/main/kotlin/com/fancymansion/core/presentation/theme/typography/TypographyMobile.kt
+++ core/presentation/src/main/kotlin/com/fancymansion/core/presentation/theme/typography/TypographyMobile.kt
@@ -19,20 +19,20 @@ val pretendard = FontFamily(
 val typographyMobile = Typography(
     // Display
     displayLarge = TextStyle(
-        lineHeight = 27.2.sp,
-        fontSize = 24.7.sp,
+        lineHeight = 26.8.sp,
+        fontSize = 24.4.sp,
         fontFamily = pretendard,
         fontWeight = FontWeight.Bold
     ),
     displayMedium = TextStyle(
-        lineHeight = 26.1.sp,
-        fontSize = 23.7.sp,
+        lineHeight = 24.1.sp,
+        fontSize = 21.9.sp,
         fontFamily = pretendard,
         fontWeight = FontWeight.Bold
     ),
     displaySmall = TextStyle(
-        lineHeight = 24.1.sp,
-        fontSize = 21.9.sp,
+        lineHeight = 22.9.sp,
+        fontSize = 20.8.sp,
         fontFamily = pretendard,
         fontWeight = FontWeight.Bold
     ),
@@ -40,23 +40,23 @@ val typographyMobile = Typography(
     // Headline
     // 컴포넌트  :
     headlineLarge = TextStyle(
-        lineHeight = 23.2.sp,
-        fontSize = 21.1.sp,
+        lineHeight = 23.3.sp,
+        fontSize = 21.2.sp,
         fontFamily = pretendard,
         fontWeight = FontWeight.Bold
     ),
 
     // 컴포넌트  : 탑바 타이틀 (태블릿)
     headlineMedium = TextStyle(
-        lineHeight = 22.3.sp,
-        fontSize = 20.3.sp,
+        lineHeight = 21.0.sp,
+        fontSize = 19.1.sp,
         fontFamily = pretendard,
         fontWeight = FontWeight.Bold
     ),
     // 컴포넌트  : 탑바 타이틀 (모바일)
     headlineSmall = TextStyle(
-        lineHeight = 20.6.sp,
-        fontSize = 18.7.sp,
+        lineHeight = 19.9.sp,
+        fontSize = 18.1.sp,
         fontFamily = pretendard,
         fontWeight = FontWeight.Bold
     ),
@@ -64,22 +64,22 @@ val typographyMobile = Typography(
     // Title
     // 컴포넌트  : 라운드 버튼 (대)
     titleLarge = TextStyle(
-        lineHeight = 19.8.sp,
-        fontSize = 18.sp,
+        lineHeight = 20.3.sp,
+        fontSize = 18.4.sp,
         fontFamily = pretendard,
         fontWeight = FontWeight.SemiBold
     ),
     // 컴포넌트  : 서브 타이틀
     titleMedium = TextStyle(
-        lineHeight = 19.sp,
-        fontSize = 17.3.sp,
+        lineHeight = 18.2.sp,
+        fontSize = 16.6.sp,
         fontFamily = pretendard,
         fontWeight = FontWeight.SemiBold
     ),
     // 컴포넌트  : 라운드 버튼 (소)
     titleSmall = TextStyle(
-        lineHeight = 17.6.sp,
-        fontSize = 16.sp,
+        lineHeight = 17.3.sp,
+        fontSize = 15.8.sp,
         fontFamily = pretendard,
         fontWeight = FontWeight.SemiBold
     ),
@@ -87,14 +87,14 @@ val typographyMobile = Typography(
     // Body
     // 컴포넌트  : 본문, 텍스트 필드, 메뉴 항목 텍스트
     bodyLarge = TextStyle(
-        lineHeight = 17.6.sp,
+        lineHeight = 17.7.sp,
         fontSize = 14.7.sp,
         fontFamily = pretendard,
         fontWeight = FontWeight.Normal
     ),
     // 컴포넌트  : 부연설명 (최대)
     bodyMedium = TextStyle(
-        lineHeight = 16.0.sp,
+        lineHeight = 15.9.sp,
         fontSize = 13.3.sp,
         fontFamily = pretendard,
         fontWeight = FontWeight.Normal
@@ -111,13 +111,13 @@ val typographyMobile = Typography(
     // 컴포넌트  : 체크박스
     labelLarge = TextStyle(
         lineHeight = 16.8.sp,
-        fontSize = 14.sp,
+        fontSize = 14.0.sp,
         fontFamily = pretendard,
         fontWeight = FontWeight.Normal
     ),
     // 컴포넌트  :
     labelMedium = TextStyle(
-        lineHeight = 15.1.sp,
+        lineHeight = 15.2.sp,
         fontSize = 12.6.sp,
         fontFamily = pretendard,
         fontWeight = FontWeight.Normal
@@ -125,7 +125,7 @@ val typographyMobile = Typography(
     // 컴포넌트  :
     labelSmall = TextStyle(
         lineHeight = 14.4.sp,
-        fontSize = 12.sp,
+        fontSize = 12.0.sp,
         fontFamily = pretendard,
         fontWeight = FontWeight.Normal
     )
