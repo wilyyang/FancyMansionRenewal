d545a83900579d339b6013646fd9a6ae4e378760
wily.yang <ehdrnr1178@gmail.com>
2024-09-10 16:14:35 +0900

    domain 모듈 테스트 코드 작성

--- domain/usecase/src/main/kotlin/com/fancymansion/domain/usecase/book/UseCaseMakeBook.kt
+++ domain/usecase/src/main/kotlin/com/fancymansion/domain/usecase/book/UseCaseMakeBook.kt
@@ -1,5 +1,6 @@
 package com.fancymansion.domain.usecase.book
 
+import com.fancymansion.core.common.const.EpisodeRef
 import com.fancymansion.core.common.const.testEpisodeRef
 import com.fancymansion.core.common.di.DispatcherIO
 import com.fancymansion.domain.interfaceRepository.BookLocalRepository
@@ -11,8 +12,8 @@ class UseCaseMakeBook @Inject constructor(
     @DispatcherIO private val dispatcher: CoroutineDispatcher,
     private val bookLocalRepository: BookLocalRepository
 ) {
-    suspend fun makeSampleEpisode() =
+    suspend fun makeSampleEpisode(episodeRef: EpisodeRef = testEpisodeRef) =
         withContext(dispatcher) {
-            bookLocalRepository.makeSampleEpisode(testEpisodeRef)
+            bookLocalRepository.makeSampleEpisode(episodeRef)
         }
 }
--- domain/usecase/src/test/kotlin/com/fancymansion/domain/usecase/book/TestBookUseCase.kt
+++ domain/usecase/src/test/kotlin/com/fancymansion/domain/usecase/book/TestBookUseCase.kt
@@ -4,6 +4,7 @@ import com.fancymansion.core.common.const.EpisodeRef
 import com.fancymansion.core.common.const.ReadMode
 import com.fancymansion.core.common.di.HiltCommon
 import com.fancymansion.di.injectRepository.HiltRepository
+import com.fancymansion.domain.model.book.PageSettingModel
 import com.google.common.truth.Truth
 import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
@@ -27,12 +28,16 @@ class TestBookUseCase {
     var hiltRule = HiltAndroidRule(this)
 
     @Inject
+    lateinit var useCasePageSetting: UseCasePageSetting
+
+    @Inject
+    lateinit var useCaseLoadBook: UseCaseLoadBook
+
+    @Inject
     lateinit var useCaseBookLogic: UseCaseBookLogic
 
-    @Before
-    fun setUp() = runTest {
-        hiltRule.inject()
-    }
+    @Inject
+    lateinit var useCaseMakeBook: UseCaseMakeBook
 
     private val defaultRef = EpisodeRef(
         userId = "test_user_id",
@@ -41,10 +46,84 @@ class TestBookUseCase {
         episodeId = "test_book_id_0"
     )
 
+    private val pageIdCountConditionRule = 3L
+    private val routeIdDefault = 6L
+
+
+    @Before
+    fun setUp() = runTest {
+        hiltRule.inject()
+        useCaseMakeBook.makeSampleEpisode(defaultRef)
+    }
+
     @Test
-    fun `Given - BookUseCase, Target - PageId, When - Update, Then - Success Equal`()  = runTest {
-        useCaseBookLogic.updateReadingProgressPageId(defaultRef, 1L)
+    fun `Given - UseCase, Target - PageSetting, When - get, Then - equal null`()  = runTest {
+        val loadSetting = useCasePageSetting.getPageSetting(defaultRef.userId, defaultRef.mode.name, defaultRef.episodeId)
+        Truth.assertThat(loadSetting == null).isTrue()
+    }
+
+    @Test
+    fun `Given - UseCase, Target - PageSetting, When - save, Then - Success equal`()  = runTest {
+        val defaultSetting = PageSettingModel()
+        useCasePageSetting.savePageSetting(defaultRef.userId, defaultRef.mode.name, defaultRef.episodeId, defaultSetting)
+        val loadSetting = useCasePageSetting.getPageSetting(defaultRef.userId, defaultRef.mode.name, defaultRef.episodeId)
+        Truth.assertThat(loadSetting == defaultSetting).isTrue()
+    }
+
+    @Test
+    fun `Given - UseCase, Target - ProgressPageId, When - get, Then - equal null`()  = runTest {
         val id = useCaseBookLogic.getReadingProgressPageId(defaultRef)
-        Truth.assertThat(id == 1L).isTrue()
+        Truth.assertThat(id == null).isTrue()
+    }
+
+    @Test
+    fun `Given - UseCase, Target - ProgressPageId, When - update, Then - Success equal`()  = runTest {
+        useCaseBookLogic.updateReadingProgressPageId(defaultRef, 3)
+        val id = useCaseBookLogic.getReadingProgressPageId(defaultRef)
+        Truth.assertThat(id!! == 3L).isTrue()
+    }
+
+    /**
+     * Test Target
+     * "pageId": 1,
+     *       "selectors": [
+     *         { "selectorId": 1, "routes": [ { "routeTargetPageId": 2  } ] },
+     *         { "selectorId": 2, "routes": [ { "routeTargetPageId": 3  } ] },
+     *         { "selectorId": 3, "routes": [ { "routeTargetPageId": 11 } ],
+     *           "showConditions": [
+     *              { "conditionId": 1, "type": "SHOW_SELECTOR",
+     *                "conditionRule": { "type": "COUNT", "count": 7, "logicalOp": "AND", "relationOp": "LESS_THAN",
+     *                                   "selfActionId": {  "pageId": 1 } }
+     *              },
+     *              { "conditionId": 2, "type": "SHOW_SELECTOR",
+     *                "conditionRule": { "type": "COUNT", "count": 1, "logicalOp": "AND", "relationOp": "GREATER_THAN",
+     *                                   "selfActionId": {  "pageId": 2 } }
+     *             },
+     *             { "conditionId": 3, "type": "SHOW_SELECTOR"
+     *               "conditionRule": { "type": "TARGET", "logicalOp": "OR", "relationOp": "LESS_THAN",
+     *                                  "selfActionId": {  "pageId": 2 },
+     *                                  "targetActionId": { "pageId": 3 } }
+     *              }
+     *           ]
+     *         }
+     *       ]
+     */
+    @Test
+    fun `Given - UseCase, Target - Logic, When - selector first, Then - Success`()  = runTest {
+        val logic = useCaseLoadBook.loadLogic(defaultRef)
+        val selectors = useCaseBookLogic.getVisibleSelectors(episodeRef = defaultRef, logic = logic, pageId = 1L)
+        Truth.assertThat(selectors.size == 2).isTrue()
+    }
+
+    @Test
+    fun `Given - UseCase, Target - Logic, When - selector condition, Then - Success`()  = runTest {
+        val logic = useCaseLoadBook.loadLogic(defaultRef)
+        useCaseBookLogic.incrementActionCount(defaultRef, 2)
+        useCaseBookLogic.incrementActionCount(defaultRef, 2)
+        useCaseBookLogic.incrementActionCount(defaultRef, 3)
+        useCaseBookLogic.incrementActionCount(defaultRef, 3)
+        useCaseBookLogic.incrementActionCount(defaultRef, 3)
+        val selectors = useCaseBookLogic.getVisibleSelectors(episodeRef = defaultRef, logic = logic, pageId = 1L)
+        Truth.assertThat(selectors.size == 3).isTrue()
     }
 }
