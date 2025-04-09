1926208d02f67bfec4cc0e8ff42e613fb6235fe0
wily.yang <ehdrnr1178@gmail.com>
2025-04-09 11:18:10 +0900

    RoundedTextField에 TextFieldValue 받는 컴포넌트 추가

--- core/presentation/src/main/kotlin/com/fancymansion/core/presentation/compose/component/TextBox.kt
+++ core/presentation/src/main/kotlin/com/fancymansion/core/presentation/compose/component/TextBox.kt
@@ -1,5 +1,6 @@
 package com.fancymansion.core.presentation.compose.component
 
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
@@ -7,6 +8,8 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.relocation.BringIntoViewRequester
+import androidx.compose.foundation.relocation.bringIntoViewRequester
 import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
@@ -16,6 +19,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -27,11 +31,13 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
@@ -39,6 +45,7 @@ import androidx.compose.ui.unit.dp
 import com.fancymansion.core.presentation.R
 import com.fancymansion.core.presentation.compose.modifier.clickSingle
 import com.fancymansion.core.presentation.compose.theme.onSurfaceSub
+import kotlinx.coroutines.launch
 
 @Composable
 fun RoundedTextField(
@@ -145,6 +152,135 @@ fun RoundedTextField(
                 modifier = Modifier.padding(start = 6.dp, end = 3.dp).height(textStyle.lineHeight.value.dp + textPadding/2).clickSingle {
                     focusRequester.requestFocus()
                     onValueChange("")
+                },
+                painter = painterResource(id = R.drawable.ic_text_cancel),
+                contentDescription = null,
+                contentScale = ContentScale.FillHeight,
+                colorFilter = ColorFilter.tint(MaterialTheme.colorScheme.outlineVariant)
+            )
+        }
+
+    }
+}
+
+@OptIn(ExperimentalFoundationApi::class)
+@Composable
+fun RoundedTextField(
+    modifier : Modifier = Modifier,
+    @androidx.annotation.IntRange(from = 1)
+    minLine : Int = 1,
+    maxLine : Int = Int.MAX_VALUE,
+
+    value : TextFieldValue,
+    textStyle : TextStyle = MaterialTheme.typography.bodyLarge,
+    hint : String = "",
+
+    imeAction : ImeAction = ImeAction.Default,
+    keyboardType : KeyboardType = KeyboardType.Text,
+    keyboardActions : KeyboardActions = KeyboardActions.Default,
+
+    rememberFocus : MutableState<Boolean> = remember { mutableStateOf(false) },
+    focusRequester : FocusRequester = remember { FocusRequester() },
+
+    borderShape : Shape = MaterialTheme.shapes.small,
+    textPadding : Dp = 13.5.dp,
+
+    isCancelable : Boolean = false,
+    isEnabled : Boolean = true,
+    onValueChange : (TextFieldValue) -> Unit,
+) {
+    val textColor = MaterialTheme.colorScheme.onSurface
+    val backgroundColor = if (isEnabled) MaterialTheme.colorScheme.surface else MaterialTheme.colorScheme.surfaceVariant
+    val borderColor = if (rememberFocus.value) {
+        MaterialTheme.colorScheme.outlineVariant
+    } else {
+        MaterialTheme.colorScheme.outline
+    }
+
+
+    val passwordVisible = rememberSaveable { mutableStateOf(keyboardType != KeyboardType.Password) }
+    val visualTransformation = if (passwordVisible.value) VisualTransformation.None else PasswordVisualTransformation()
+
+    val paddingSize = with(LocalDensity.current) { 24.dp.toPx() }
+    val bringIntoViewRequester = remember { BringIntoViewRequester() }
+    val scope = rememberCoroutineScope()
+
+    Row(
+        // 보더 영역
+        modifier = modifier
+            .border(
+                width = 1.dp,
+                color = borderColor,
+                shape = borderShape
+            )
+            .padding(0.5.dp)
+            .clip(
+                shape = borderShape
+            )
+            .background(backgroundColor).padding(start = textPadding, end = 8.dp)
+            .focusRequester(focusRequester = focusRequester),
+
+        verticalAlignment = Alignment.CenterVertically) {
+
+        // 입력 영역
+        BasicTextField(
+            modifier = Modifier.weight(1f)
+                .padding(vertical = textPadding)
+                .onFocusChanged {rememberFocus.value = it.isFocused }
+                .bringIntoViewRequester(bringIntoViewRequester),
+            enabled = isEnabled,
+
+            singleLine = maxLine == 1,
+            minLines = minLine,
+            maxLines = maxLine,
+            value = value,
+            textStyle = textStyle.copy(color = textColor),
+
+            visualTransformation = visualTransformation,
+            keyboardOptions = KeyboardOptions.Default.copy(
+                keyboardType = keyboardType,
+                imeAction = imeAction
+            ),
+            keyboardActions = keyboardActions,
+            decorationBox = { innerTextField ->
+
+                // 힌트 영역
+                if (value.text.isEmpty() && hint.isNotEmpty()) {
+                    Text(text = hint,  maxLines = maxLine, style = textStyle.copy(color = onSurfaceSub))
+                }
+                innerTextField()
+            },
+            onTextLayout = {
+                val cursorRect = it.getCursorRect(value.selection.start)
+                val paddedCursorRect = cursorRect.inflate(paddingSize)
+                scope.launch {
+                    bringIntoViewRequester.bringIntoView(paddedCursorRect)
+                }
+            },
+            onValueChange = onValueChange,
+        )
+
+        if (keyboardType == KeyboardType.Password) {
+            Image(
+                modifier = Modifier.padding(start = 6.dp, end = 3.dp).height(textStyle.lineHeight.value.dp + textPadding/2).clickSingle {
+                    focusRequester.requestFocus()
+                    passwordVisible.value = !passwordVisible.value
+                },
+                painter = painterResource(id = if (passwordVisible.value) {
+                    R.drawable.ic_text_password_hide
+                } else {
+                    R.drawable.ic_text_password_show
+                }),
+                contentDescription = null,
+                contentScale = ContentScale.FillHeight,
+                colorFilter = ColorFilter.tint(MaterialTheme.colorScheme.outlineVariant)
+            )
+
+        }else if(isCancelable && value.text.isNotEmpty()){
+            Image(
+                modifier = Modifier.padding(start = 6.dp, end = 3.dp).height(textStyle.lineHeight.value.dp + textPadding/2).clickSingle {
+                    focusRequester.requestFocus()
+                    onValueChange(TextFieldValue(text = ""))
                 },
                 painter = painterResource(id = R.drawable.ic_text_cancel),
                 contentDescription = null,
