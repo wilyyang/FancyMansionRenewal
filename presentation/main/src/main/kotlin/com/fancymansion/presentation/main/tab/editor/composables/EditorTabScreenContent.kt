2af31f44027375b28fcc390702ffd7dbc9795f4e
wily.yang <ehdrnr1178@gmail.com>
2025-08-04 09:24:04 +0900

    메인 에디터 탭 UI 대략 구성

--- presentation/main/src/main/kotlin/com/fancymansion/presentation/main/tab/editor/composables/EditorTabScreenContent.kt
+++ presentation/main/src/main/kotlin/com/fancymansion/presentation/main/tab/editor/composables/EditorTabScreenContent.kt
@@ -2,17 +2,24 @@ package com.fancymansion.presentation.main.tab.editor.composables
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight.Companion.Medium
+import androidx.compose.ui.text.font.FontWeight.Companion.SemiBold
 import androidx.compose.ui.unit.dp
 import com.fancymansion.core.common.resource.StringValue
 import com.fancymansion.core.presentation.base.CommonEvent
@@ -20,6 +27,7 @@ import com.fancymansion.core.presentation.compose.component.EnumDropdown
 import com.fancymansion.core.presentation.compose.component.RoundedTextField
 import com.fancymansion.core.presentation.compose.screen.NoDataScreen
 import com.fancymansion.core.presentation.compose.shape.borderLine
+import com.fancymansion.core.presentation.compose.theme.onSurfaceDimmed
 import com.fancymansion.presentation.main.tab.editor.EditorTabContract
 import com.fancymansion.presentation.main.R
 import java.text.SimpleDateFormat
@@ -61,18 +69,22 @@ fun EditorTabScreenContent(
             )
         }
     } else {
-        val testListData = listOf(BookHolderData(
-            title = "나는 왜 남들보다 쉽게 지칠까",
-            editDate = 1752105600000L,
-            pageCount = 21,
-            keywords = listOf("희망", "절망", "리얼리즘")
-        ),
-            BookHolderData(
-                title = "나는 왜 남들보다 쉽게 지칠까",
-                editDate = 1752105600000L,
-                pageCount = 21,
-                keywords = listOf("희망", "절망", "리얼리즘")
-            ))
+        val testListData = mutableListOf<BookHolderData>()
+        for (i in 0..30) {
+            testListData.add(
+                BookHolderData(
+                    title = "나는 왜 남들보다 쉽게 지칠까 $i",
+                    editDate = 1752105600000L,
+                    pageCount = 21,
+                    keywords = listOf("희망", "절망", "리얼리즘")
+                )
+            )
+        }
+
+        val list1 = testListData.subList(0, 10)
+        val list2 = testListData.subList(10, 20)
+        val list3 = testListData.subList(20, 30)
+
         Column(
             modifier = Modifier
                 .fillMaxSize()
@@ -149,10 +161,39 @@ fun EditorTabScreenContent(
                     }
                 }
 
-                items(testListData){ data ->
+                itemsIndexed (list1){ idx, data ->
                     EditBookHolder(
                         bookHolderData = data
                     )
+
+                    if (idx < list1.size - 1) {
+                        HorizontalDivider(
+                            modifier = Modifier
+                                .padding(horizontal = 14.dp)
+                                .height(0.3.dp)
+                                .fillMaxWidth(),
+                            color = MaterialTheme.colorScheme.outline
+                        )
+                    }
+                }
+
+                item{
+                    Row(
+                        modifier = Modifier
+                            .padding(vertical = 50.dp)
+                            .fillMaxWidth(),
+                        verticalAlignment = Alignment.CenterVertically,
+                        horizontalArrangement = Arrangement.Center
+                    ){
+                        for(i in 1 .. 3){
+                            Text(
+                                modifier = Modifier.padding(end = if(i != 3) 30.dp else 0.dp),
+                                text = "$i",
+                                style = MaterialTheme.typography.bodyLarge.copy(fontWeight = if(i == 1) SemiBold else Medium),
+                                color = if(i == 1) MaterialTheme.colorScheme.onSurface else onSurfaceDimmed
+                            )
+                        }
+                    }
                 }
             }
         }
@@ -169,14 +210,18 @@ fun EditBookHolder(
         verticalAlignment = Alignment.CenterVertically
     ){
         Image(
-            modifier = Modifier.fillMaxWidth(0.25f).heightIn(max = 200.dp),
+            modifier = Modifier
+                .fillMaxWidth(0.25f)
+                .heightIn(max = 200.dp),
             painter = painterResource(id = R.drawable.holder_book_image_sample),
             contentScale = ContentScale.FillWidth,
             contentDescription = ""
         )
 
         Column(
-            modifier = Modifier.fillMaxWidth(1f).padding(start = 10.dp)
+            modifier = Modifier
+                .fillMaxWidth(1f)
+                .padding(start = 10.dp)
         ) {
             Text(
                 text = bookHolderData.title,
@@ -184,9 +229,39 @@ fun EditBookHolder(
             )
 
             Text(
+                modifier = Modifier.padding(top = 5.dp),
                 text = "${formatTimestampLegacy(bookHolderData.editDate)} 편집됨",
                 style = MaterialTheme.typography.bodyLarge
             )
+
+            Text(
+                modifier = Modifier.padding(top = 5.dp),
+                text = "총 ${bookHolderData.pageCount} 페이지",
+                style = MaterialTheme.typography.bodyLarge,
+                color = MaterialTheme.colorScheme.primary
+            )
+
+            Row(
+                modifier = Modifier.padding(top = 5.dp)
+            ) {
+                bookHolderData.keywords.forEach { keyword ->
+                    Text(
+                        modifier = Modifier
+                            .padding(end = 2.dp)
+                            .clip(shape = MaterialTheme.shapes.extraSmall)
+                            .padding(0.5.dp)
+                            .border(
+                                width = 0.5.dp,
+                                color = MaterialTheme.colorScheme.outline,
+                                shape = MaterialTheme.shapes.extraSmall
+                            )
+                            .padding(horizontal = 7.dp, vertical = 5.dp),
+                        text = "#$keyword",
+                        style = MaterialTheme.typography.bodySmall,
+                        color = Color.Black
+                    )
+                }
+            }
         }
     }
 }
